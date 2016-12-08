d68b3b961ad7a7b1de45d578146ac496104b50d0
anddani <anddani@kth.se>
2016-12-08 10:39:41 +0100

    Done with day 8 part 1

---
+++ andre/2016/08/08.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my @screen;
+push @screen, [(0) x 50] for 1..6;
+my $num_rows = scalar(@screen);
+
+while (<>) {
+    chomp $_;
+
+    if ($_ =~ m/rect (\d+)x(\d+)/) {
+        for my $x (0..($1-1)) {
+            for my $y (0..($2-1)) {
+                $screen[$y]->[$x] = 1;
+            }
+        }
+    } elsif ($_ =~ m/rotate.*(\w)=(\d+) by (\d+)/) {
+        if ($1 eq "y") {
+            unshift @{$screen[$2]}, pop @{$screen[$2]} for 1..$3;
+        } elsif ($1 eq "x") {
+            for (1..$3) {
+                my $temp = $screen[$num_rows-1]->[$2];
+                for my $i (reverse 1..($num_rows-1)) {
+                    $screen[$i]->[$2] = $screen[$i-1]->[$2];
+                }
+                $screen[0]->[$2] = $temp;
+            }
+        }
+    }
+}
+
+my $sum = 0;
+map { $sum += scalar(grep { $_ == 1 } @{$_}) } @screen;
+print "$sum\n";
