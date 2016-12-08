2cd051991fb94c87b6fa55a72d8fab3699400b20
anddani <anddani@kth.se>
2016-12-08 10:48:55 +0100

    Done with day 8

--- andre/2016/08/08.pl
+++ andre/2016/08/08.pl
@@ -12,16 +12,20 @@ my $num_rows = scalar(@screen);
 while (<>) {
     chomp $_;
 
+    # Fill rectangle with NxM lights
     if ($_ =~ m/rect (\d+)x(\d+)/) {
         for my $x (0..($1-1)) {
             for my $y (0..($2-1)) {
                 $screen[$y]->[$x] = 1;
             }
         }
+    # Rotate column or row
     } elsif ($_ =~ m/rotate.*(\w)=(\d+) by (\d+)/) {
         if ($1 eq "y") {
+            # Rotate row
             unshift @{$screen[$2]}, pop @{$screen[$2]} for 1..$3;
         } elsif ($1 eq "x") {
+            # Rotate column
             for (1..$3) {
                 my $temp = $screen[$num_rows-1]->[$2];
                 for my $i (reverse 1..($num_rows-1)) {
@@ -34,5 +38,12 @@ while (<>) {
 }
 
 my $sum = 0;
-map { $sum += scalar(grep { $_ == 1 } @{$_}) } @screen;
+map { $sum += scalar(grep { $_ } @{$_}) } @screen;
 print "$sum\n";
+
+for my $row (0..5) {
+    for my $col (0..49) {
+        print $screen[$row]->[$col] ? '#' : '.';
+    }
+    print "\n";
+}
