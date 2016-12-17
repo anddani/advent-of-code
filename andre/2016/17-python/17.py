80d2db1ce7ccc722eb4e7cc0127f3bc0dd6942f8
anddani <anddani@kth.se>
2016-12-17 09:50:54 +0100

    quotes

--- andre/2016/17-python/17.py
+++ andre/2016/17-python/17.py
@@ -8,9 +8,9 @@ import sys
 passcode = sys.stdin.readline().strip()
 open_doors = set(['b', 'c', 'd', 'e', 'f'])
 dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-dirs_c = ["U", "D", "L", "R"]
+dirs_c = ['U', 'D', 'L', 'R']
 
-queue = deque([["", (0, 0)]])
+queue = deque([['', (0, 0)]])
 paths = []
 
 while len(queue) > 0:
@@ -24,5 +24,5 @@ while len(queue) > 0:
             if next_pos[0] in range(4) and next_pos[1] in range(4):
                 if hash[i] in open_doors:
                     queue.append([curr_path+dirs_c[i], next_pos])
-print("Part 1: ", paths[0][0])
-print("Part 2: ", paths[-1][1])
+print('Part 1: ', paths[0][0])
+print('Part 2: ', paths[-1][1])
