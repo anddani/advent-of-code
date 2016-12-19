d9617a37e0ecbee77659bf153aa94023ca7e98dc
wixenius <wixenius@users.noreply.github.com>
2016-12-19 12:29:13 +0100

    Done with part 1 day 19.

---
+++ Fredrik/2016/1_19.py
@@ -0,0 +1,22 @@
+
+puzzle = 5
+puzzle = 3017957
+
+# Creat ring, but exclude all Elf that will have 
+# their present stolen the first iteration.
+# We start at 3 since the input is odd.
+ring = range(3,puzzle+1,2)
+no_presents = set()
+
+while len(ring) != 1:
+    for i in range(len(ring)):
+        # Only let the Elf steal present if it has presents.
+        if ring[i] not in no_presents:
+            if i+1 < len(ring):
+                no_presents.add(ring[i+1])
+            else:
+                no_presents.add(ring[0])
+    # Exclude all elfs that no longer have any presents.
+    ring = [elf for elf in ring if elf not in no_presents]
+    
+print(ring[0])
