69ee3fd3be5564bd0f5c8c9108b52215c028f30e
anddani <anddani@kth.se>
2016-12-20 09:02:03 +0100

    Minor

--- andre/2016/20/20.pl
+++ andre/2016/20/20.pl
@@ -30,6 +30,6 @@ my $sum = 0;
 for my $intervals (@sorted) {
     # Add gap to sum
     $sum += @{$intervals}[0] - $curr_e if ($curr_e < @{$intervals}[0]);
-    $curr_e = @{$intervals}[1]+1 if ($curr_e <= @{$intervals}[1]);
+    $curr_e = max($curr_e, @{$intervals}[1]+1);
 }
 print "Part 2: ", $largest + 1 + $sum - $curr_e, "\n";
