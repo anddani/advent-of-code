50ecd2d662c5020b23f8c0f56904f9b52741125f
anddani <anddani@kth.se>
2016-12-26 14:19:11 +0100

    Done with part 1

--- andre/2016/11-ruby/11.rb
+++ andre/2016/11-ruby/11.rb
@@ -14,47 +14,68 @@ end
 
 # Valid state is when there are matching generator and chips
 # or 
-def valid?(state)
-  state.each do |f|
-    chips = f.select{ |item| item[1] == "M" }
-    generators = f.select{ |item| item[1] == "G" }
-    chips.each do |c|
-      if not generators.include?(c[0]+"G") and generators.count > 0
-        return false
-      end
-    end
+def valid?(floor)
+  chips, generators = floor.partition{ |item| item[1] == "M" }
+  return true if generators.empty?
+
+  # All chips has a matching generator
+  return chips.all? do |c|
+    generators.any? { |g| g[0] == c[0] }
   end
-  return true
 end
 
-num_elems = floors.flatten.count
-visited = Set[floors]
-queue = [[0, 0, floors]]
-best = 0
+def repr(state)
+  return state.map { |s| Set.new(s) }
+end
+
+floors.map! { |f| Set.new(f) }
+
+s = [0, floors]
+num_elems = floors.reduce(0) { |sum, e| sum += e.size }
+distance = Hash.new(Float::INFINITY)
+distance[s] = 0
+queue = PriorityQueue.new
+queue.insert(0, s)
 
 until queue.empty?
-  curr_floor, steps, state = queue.shift
-  if state[3].count == num_elems
-    best = steps
+  curr = queue.min
+  f, state = curr
+  items = state[f]
+
+  if f == 3 and items.count == num_elems
+    puts distance[curr]
+    p state
     break
   end
 
-  state.each_with_index do |f, i|
-    combinations = f.combination(1).to_a + f.combination(2).to_a
-    [1, -1].each do |dir|
-      if (i+dir).between?(0, 3)
-        combinations.each do |c|
-          new_state = state.dup
-          new_state[i] -= c
-          new_state[i+dir] += c
-          if valid? new_state
-            visited.add(new_state)
-            queue.push([i, (i-curr_floor).abs + steps+1, new_state])
+  moves = items.to_a.combination(2).to_a + items.to_a.combination(1).to_a + []
+
+  moves.select! { |m| valid?(items-m) }
+
+  [-1, 1].each do |dir|
+    new_f = f+dir
+
+    if new_f.between?(0, 3)
+
+      moves.each do |move|
+        new_floor = state[new_f] + move
+
+        if valid?(new_floor)
+
+          new_state = state.map(&:dup)
+          new_state[f] = items - move
+          new_state[new_f] = new_floor
+
+          e = [new_f, new_state]
+          new_cost = distance[curr] + 1
+          if distance[e] == Float::INFINITY or new_cost < distance[e]
+            distance[e] = new_cost
+            # Bad heuristic?
+            prio = new_cost + new_state[3].count + 3*new_state[0].count + 2*new_state[1].count + new_state[2].count
+            queue.insert(new_cost, e)
           end
         end
       end
     end
   end
 end
-
-puts best
--- andre/2016/11-ruby/priority_queue.rb
+++ andre/2016/11-ruby/priority_queue.rb
@@ -1,42 +1,29 @@
 class PriorityQueue
-  attr_accessor :elements
-
   def initialize
-    @elements = [nil]
+    @elements = Hash.new { |h, k| h[k] = [] }
   end
 
-  def <<(element)
-    @elements << element
-    b_up(@elements.size - 1)
+  def insert(prio, data)
+    @elements[prio] << data
   end
 
-  def b_up(index)
-    p_index = index/2
-    return if index <= 1 or @elements[p_index] >= @elements[index]
-    exchange(index, p_index)
-    b_up(p_index)
+  def min
+    return nil if @elements.empty?
+    key, val = *@elements.min
+    result = val.shift
+    @elements.delete(key) if val.empty?
+    return result
   end
 
-  def exchange(s, t)
-    @elements[s], @elements[t] = @elements[t], @elements[s]
+  def include?(data)
+    return @elements.values.flatten.include? data
   end
 
-  def pop
-    exchange(1, @elements.size - 1)
-    max = @elements.pop
-    b_down(1)
-    return max
+  def empty?
+    return @elements.empty?
   end
 
-  def b_down(index)
-    child_index = (index*2)
-    return if child_index > @elements.size - 1
-    is_not_last = child_index < @elements.size - 1
-    left_elem = @elements[child_index]
-    right_elem = @elements[child_index + 1]
-    child_index += 1 if is_not_last and right_elem > left_elem
-    return if @elements[index] >= @elements[child_index]
-    exchange(index, child_index)
-    b_down(child_index)
+  def size
+    return @elements.size
   end
 end
