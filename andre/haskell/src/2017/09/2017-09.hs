326fecf1db3f7a823b9be1b7355b91553cfb9cd6
André Danielsson <andre.danielsson@bontouch.com>
2017-12-18 15:19:31 +0100

    Day 9 2017 part 1

---
+++ andre/haskell/src/2017/09/2017-09.hs
@@ -0,0 +1,40 @@
+import Control.Applicative ((<$), (<*), (*>))
+import Control.Monad ((>>))
+import Data.Either.Unwrap
+import Data.List
+import Text.ParserCombinators.Parsec
+
+data Stream = Garbage String | Group [Stream]
+    deriving Show
+
+parseGarbageContent :: Parser String
+parseGarbageContent =
+    (char '!' >> anyChar >> return "")
+    <|> (:[]) <$> noneOf ">"
+
+parseGarbage :: Parser Stream
+parseGarbage =
+    between (char '<') (char '>') $
+        Garbage <$> (concat <$> many parseGarbageContent)
+
+parseGroup :: Parser Stream
+parseGroup =
+    between (char '{') (char '}') $
+        Group <$> parseStream `sepBy` char ','
+
+parseStream :: Parser Stream
+parseStream = parseGroup <|> parseGarbage
+
+parseInput :: String -> Stream
+parseInput = fromRight . parse parseStream ""
+
+solveOne :: Int -> Stream -> Int
+solveOne depth (Group gs) = depth + sum (map (solveOne (depth + 1)) gs)
+solveOne _ _ = 0
+
+main :: IO ()
+main = do
+    input <- readFile "inputs/2017-09"
+    let stream = parseInput (init input)
+    print $ solveOne 1 stream
+    -- print $ solveTwo input
