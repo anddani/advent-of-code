f8149c905eace333eb6697102661957432abbb8a
trangle1302 <lengochaitrang@gmail.com>
2021-09-15 17:03:51 +0200

    day5 part 2

--- trang/rust-2020/src/days/d05.rs
+++ trang/rust-2020/src/days/d05.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead,BufReader};
+use itertools::izip;
+use std::ops::Range;
 
 pub fn run() {
 
@@ -9,15 +11,27 @@ pub fn run() {
     The fold method takes an initial value (0 in here) and a closure with two arguments: an ‘accumulator’, and an element
     acc + 1 if half is B or R
     */
-    let max_id = file.lines()
-    .filter_map(Result::ok) //pass through a sequence of Results and yield only the “successful” values
-    .map(|l| {
-        l.chars().fold(0, |acc, half| {
-            (acc << 1) | matches!(half, 'B' | 'R') as u16 //u16: integer 0 to 2^16-1
+    
+    let mut seats: Vec<_> = file.lines()
+        .filter_map(Result::ok)
+        .map(|l| {
+            l.chars().fold(0, |acc, half| {
+                (acc << 1) | matches!(half, 'B' | 'R') as u16
+            })
         })
-    })
-    .max()
-    .expect("Not empty");
+        .collect();
+    seats.sort(); //ascending
+    let max_id = seats.last().unwrap().clone();
     println!("Highest seat ID: {}", max_id);
-
+    
+    let mut myseat = 0;
+    for (i, id) in izip!(Range { start: seats[0], end: max_id }, seats.iter()) {
+        //seats at the front and back of the planes are not include
+        //println!("{} {}", i, id);
+        if i != *id {
+            myseat = *id - 1;
+            break;
+        }
+    }
+    println!("First seat ID to break pattern: {}", myseat);
 }
