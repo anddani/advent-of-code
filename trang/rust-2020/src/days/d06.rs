653d9d9ae64cdc33b8419ad51271643fa8ea2001
trangle1302 <lengochaitrang@gmail.com>
2021-09-18 08:22:55 +0200

    day6 part 2

--- trang/rust-2020/src/days/d06.rs
+++ trang/rust-2020/src/days/d06.rs
@@ -25,25 +25,44 @@ pub fn run() {
     let f = BufReader::new(File::open("./data/input_d06.txt").unwrap());
 
     let mut counts = 0;
+    let mut counts2 = 0;
     let mut set = HashSet::new();
+    let mut set2 = HashSet::new();
+    let mut start_chunk = true;
     for l in f.lines() {
         let l = l.unwrap();
         if l.is_empty() {
             counts += set.len();
             println!("Line break! Set len: {}", set.len());
             set.clear();  
+            
+            counts2 += set2.len();
+            set2.clear();
+            start_chunk = true;
         } else {
             set.extend(l.chars()); //Extends a collection with the contents of an iterator
             println!("{}",l);
+            if start_chunk {
+                set2.extend(l.chars());
+                start_chunk = false;
+            } else {
+                let mut current = HashSet::new();
+                current.extend(l.chars());
+                set2.retain(|&k| current.contains(&&k));
+            }
         }
     }
     if set.len() > 0 {
         counts += set.len();
-        println!("Last lines: Set len: {}", set.len());
+        println!("Last lines: Set1 len: {}", set.len());
         set.clear();  
     }
-    println!("Count {}", counts);
+    if set2.len() > 0 {
+        counts2 += set2.len();
+        println!("Last lines: Set2 len: {}", set2.len());
+        set2.clear();  
+    }
+    println!("Count union {}", counts);
+    println!("Count intersection {}", counts2);
 
-    
-    
 }
