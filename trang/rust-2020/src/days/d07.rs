61db5471fee41f9bf444da6e15ad86543b50d1a3
trangle1302 <lengochaitrang@gmail.com>
2021-09-19 20:51:05 +0200

    day7 part 1

--- trang/rust-2020/src/days/d07.rs
+++ trang/rust-2020/src/days/d07.rs
@@ -3,33 +3,41 @@ use std::io::{BufRead, BufReader};
 use std::time::Instant;
 use petgraph::graphmap::DiGraphMap;
 use regex::Regex;
-
-fn graph(lines: &[String]) -> DiGraphMap<&str, usize> {
-    let re_node0 = Regex::new(r"(\w+) bags contain").unwrap();
-    let re_node1 = Regex::new(r"(\d+) (\w+) bag").unwrap();
-    let mut g = DiGraphMap::new();
-    for l in lines {
-        let bag0 = re_node0.captures(&l).unwrap().get(1).unwrap().as_str();
-        let bags1 = re_node1.captures_iter(&l);
-        for b in bags1 {
-            let n: usize = b[1].parse().unwrap();
-            let bag = b.get(2).unwrap().as_str();
-            g.add_edge(bag0, bag, n);
-            println!{"{}: {} {}", bag0,bag, n};
-        }
-    }
-    return g;
-}
+use petgraph::algo::has_path_connecting;
 
 pub fn run() {
     let time = Instant::now();
-    let f = BufReader::new(File::open("./data/test.txt").unwrap()); //read_to_string("./data/test.txt").unwrap();
-    let lines: Vec<String> = f.lines().map(|l| l.unwrap()).collect();
-    let g = graph(&lines);
-    assert!(g.contains_edge("dark orange", "shiny gold"));
-    let count1 = 0; //g.neighbors_directed("shiny gold")
-    println!("N bags that can contain at least 1 shiny gold bag {}", count1);
 
+    let f = BufReader::new(File::open("./data/input_d07.txt").unwrap()); //read_to_string("./data/test.txt").unwrap();
+    let lines: Vec<String> = f.lines().collect::<Result<_, _>>().unwrap();
+
+    // Construct a directed graph where nodes are colors and weights are the number bags a upper node/bag can hold
+    let mut g = DiGraphMap::new();
+    let re_node0 = Regex::new(r"([\w ]+) bags contain").unwrap();
+    let re_node1 = Regex::new(r"(\d+) ([\w ]+) bags?").unwrap();
+    for l in &lines {
+        let bag0 = re_node0.captures(&l).unwrap().get(1).unwrap().as_str(); //`&str`
+        for b in re_node1.captures_iter(&l) {
+            let n: usize = b.get(1).unwrap().as_str().parse().unwrap();
+            let bag1 = b.get(2).unwrap().as_str(); //`&str`
+            g.add_edge(bag0, bag1, n);
+            println!{"{}: {} {}", bag0, bag1, n};
+        }
+    }
+
+    // Part 1
+    let mut count1 = 0;
+    println!("Incoming nodes:");
+    for node in g.nodes() {
+        println!("Node: {:?}", node);
+        if node != "shiny gold" {
+            if has_path_connecting(&g, node, "shiny gold", None) {
+                count1 += 1;
+            }
+        }
+    }
+    println!("N bags that can contain at least 1 shiny gold bag {}", count1);
+    
     let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
     println!("Elapsed: {:.3} ms", elapsed_ms);
 }
