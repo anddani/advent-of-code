7fc03a81661dcf4493c643ec789353c10d250b2d
trangle1302 <lengochaitrang@gmail.com>
2021-09-22 23:48:21 +0200

    day9 part 2, failed attempt at wrapping parts into fn

--- trang/rust-2020/src/days/d09.rs
+++ trang/rust-2020/src/days/d09.rs
@@ -1,25 +1,62 @@
-pub fn run() {
-    let stdin = std::fs::read_to_string("./data/input_d09.txt").unwrap();
-    let nums: Vec<i64> = stdin.lines()
-        .map(|line| line.parse().unwrap())
-        .collect(); 
-    let window_size: usize = 25;
-    for (i, window) in nums.windows(window_size+1).enumerate() {
-        if i < (window_size - 1) {
+/*
+fn part_1(nums: &Vec<i64>) -> i64 {
+    let ws: usize = 25;
+    for (i, window) in nums.windows(ws+1).enumerate() {
+        if i < (ws - 1) {
             continue
         }
         let mut valid = 0;
-        //println!("{:?}", window);
-        for i1 in 0..(window_size-1) {
-            for i2 in (1+i1)..window_size {
-                if window[i1] + window[i2] == window[window_size] {
+        println!("{:?}", window);
+        for i1 in 0..(ws-1) {
+            for i2 in (1+i1)..ws {
+                if window[i1] + window[i2] == window[ws] {
                     valid += 1;
                 }
             }
         }
         if valid == 0 {
-            println!("First number to break: {:?}", window[window_size]);
+            println!("First number to break: {:?}", window[ws]);
+            return *window[ws]
+        }
+    }
+}
+*/
+pub fn run() {
+    let stdin = std::fs::read_to_string("./data/input_d09.txt").unwrap();
+    let nums: Vec<i64> = stdin.lines()
+        .map(|line| line.parse().unwrap())
+        .collect(); 
+
+    //let solution_1 = part_1(&nums);
+    //println!("{}", solution_1);
+    let mut solution_1: i64 = 0;
+    let ws: usize = 25;
+    for (i, window) in nums.windows(ws+1).enumerate() {
+        if i < (ws - 1) {
+            continue
+        }
+        let mut valid = 0;
+        for i1 in 0..(ws-1) {
+            for i2 in (1+i1)..ws {
+                if window[i1] + window[i2] == window[ws] {
+                    valid += 1;
+                }
+            }
+        }
+        if valid == 0 {
+            println!("First number to break: {:?}", window[ws]);
+            solution_1 = window[ws];
             break
         }
+    }
+
+    for ws2 in 2.. {
+        for (i, window) in nums.windows(ws2+1).enumerate() {
+            if window.iter().sum::<i64>() == solution_1 {
+                let solution_2 = window.iter().min().unwrap() + window.iter().max().unwrap();
+                println!("Encryption weakness: {:?}", solution_2);
+                break
+            }
+        }    
     }
 }
