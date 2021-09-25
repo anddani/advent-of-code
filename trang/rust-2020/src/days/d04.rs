51ca678bda3e03a2543358f841b5a7e161c8f2d2
trangle1302 <lengochaitrang@gmail.com>
2021-09-25 09:31:55 +0200

    d4 p1 into fn

--- trang/rust-2020/src/days/d04.rs
+++ trang/rust-2020/src/days/d04.rs
@@ -1,12 +1,8 @@
 use regex::Regex;
 use std::collections::HashSet;
-
-pub fn run() {
-    let stdin = std::fs::read_to_string("./data/input_d04.txt").unwrap();
-    let ps = stdin.split("\n\n").collect::<Vec<&str>>();
+fn count_all(ps:&Vec<&str>, sets: HashSet<&str>) -> usize{
     let re_p = Regex::new(r"(\w+{3}):(\S+)").unwrap();
-    let sets: HashSet<&str> = ["byr","iyr","eyr","hgt","hcl","ecl","pid"].iter().cloned().collect();
-    let mut vp = 0;
+    let mut vp: usize = 0;
     for p in ps {
         let caps = re_p.captures_iter(&p);
         let caps_id: HashSet<&str> = caps.map(|c| c.get(1).unwrap().as_str()).collect();
@@ -16,6 +12,14 @@ pub fn run() {
             vp += 1;
         }
     }
-    
+    return vp
+}
+
+
+pub fn run() {
+    let stdin = std::fs::read_to_string("./data/input_d04.txt").unwrap();
+    let ps = stdin.split("\n\n").collect::<Vec<&str>>();
+    let sets: HashSet<&str> = ["byr","iyr","eyr","hgt","hcl","ecl","pid"].iter().cloned().collect();
+    let vp = count_all(&ps, sets);
     println!("Valid passports: {}", vp)
 }
