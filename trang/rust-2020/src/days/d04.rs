1d993a15763c636a2b270bb3f00627f461a60446
trangle1302 <lengochaitrang@gmail.com>
2021-10-02 21:48:11 +0200

    cleanup

--- trang/rust-2020/src/days/d04.rs
+++ trang/rust-2020/src/days/d04.rs
@@ -2,21 +2,6 @@ use regex::Regex;
 use std::collections::HashSet;
 use std::time::Instant;
 
-fn count_all(ps:&Vec<&str>, sets: HashSet<&str>) -> usize{
-    let re_p = Regex::new(r"(\w+{3}):(\S+)").unwrap();
-    let mut vp: usize = 0;
-    for p in ps {
-        let caps = re_p.captures_iter(&p);
-        let caps_id: HashSet<&str> = caps.map(|c| c.get(1).unwrap().as_str()).collect();
-        let intersection: HashSet<&&str> = sets.intersection(&caps_id).collect();
-        
-        if intersection.len() >= 7 {
-            vp += 1;
-        }
-    }
-    return vp
-}
-
 fn check_valid(k: &str, v: &str) -> bool {
     let re_hgt = Regex::new(r"^(\d+)(cm|in)$").unwrap();
     let re_hcl = Regex::new(r"^#([0-9a-f]{6})$").unwrap();
@@ -58,8 +43,6 @@ pub fn run() {
     let stdin = std::fs::read_to_string("./data/input_d04.txt").unwrap();
     let ps = stdin.split("\n\n").collect::<Vec<&str>>();
     let sets: HashSet<&str> = ["byr","iyr","eyr","hgt","hcl","ecl","pid"].iter().cloned().collect();
-    //let vp = count_all(&ps, sets);
-    //println!("Valid passports: {}", vp)
 
     let re_p = Regex::new(r"(\w+{3}):(\S+)").unwrap();
     let mut vp: usize = 0;
@@ -81,7 +64,9 @@ pub fn run() {
             }
         }
     }
-    println!("Valid passports: {}", vp2);
+    
+    println!("Valid passports part 1: {}", vp);
+    println!("Valid passports part 2: {}", vp2);
     
     let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
     println!("Elapsed: {:.3} ms", elapsed_ms);
