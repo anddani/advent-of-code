36d98e4dbbb2249d8d582dced28c7ba522f0deec
trangle1302 <lengochaitrang@gmail.com>
2021-12-20 11:51:57 +0100

    d1 p2

--- trang/rust-2021/d1/src/main.rs
+++ trang/rust-2021/d1/src/main.rs
@@ -10,4 +10,16 @@ fn main() {
         }
     }
     println!("number of increased measurement: {}", n_increase);
+
+    let ws = 3;
+    let mut previous_sum = 0;
+    let mut n_increase2 = -1; // compensate for the first comparision, which will result in false positve
+    for (i, window) in n.windows(ws).enumerate() {
+        let current_sum = window.iter().sum(); 
+        if current_sum > previous_sum {
+            n_increase2 += 1;
+        }
+        previous_sum = current_sum;
+    }
+    println!("number of increased sum of 3: {}", n_increase2);
 }
