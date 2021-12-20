f35f3ab6296e8b36fea1ac08f81e064c825280fc
trangle1302 <lengochaitrang@gmail.com>
2021-12-20 12:34:35 +0100

    d2 p2

--- trang/rust-2021/d2/src/main.rs
+++ trang/rust-2021/d2/src/main.rs
@@ -1,6 +1,5 @@
-fn main() {
+fn part1() {
     let f = std::fs::read_to_string("./input.txt").unwrap();
-
     let mut depth = 0;
     let mut horizontal = 0;
     for l in f.lines() {
@@ -12,7 +11,32 @@ fn main() {
             "down" => depth += stepsize,
             _ => panic!("Not yet implemented: {:?}", v[0]),
         }
-        println!("{:?} {}", v[0], stepsize);
     }
     println!("Manhattan distance: {}", depth*horizontal);
+}
+
+fn part2() {    
+    let f = std::fs::read_to_string("./input.txt").unwrap();
+
+    let mut depth = 0;
+    let mut horizontal = 0;
+    let mut aim = 0;
+    for l in f.lines() {
+        let v: Vec<&str> = l.split(' ').collect();
+        let stepsize: i32 = v[1].parse().unwrap();
+        match v[0] {
+            "forward" => {
+                horizontal += stepsize;
+                depth += aim*stepsize
+            },
+            "up" => aim -= stepsize,
+            "down" => aim += stepsize,
+            _ => panic!("Not yet implemented: {:?}", v[0]),
+        }
+    }
+    println!("Manhattan distance with aim: {}", depth*horizontal);
+}
+fn main() {
+    part1();
+    part2();
 }
