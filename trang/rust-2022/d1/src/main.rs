441698567323394fe2c34db5abfb52904c447ae3
trangle1302 <lengochaitrang@gmail.com>
2022-12-03 14:04:34 -0800

    d1 p1,2

--- trang/rust-2022/d1/src/main.rs
+++ trang/rust-2022/d1/src/main.rs
@@ -3,14 +3,14 @@ fn main() {
     let f = std::fs::read_to_string("./input.txt").unwrap();
     let elfs = f.split("\n\n");
     
-    let mut highest_calorie = 0;
-    for p in elfs {
-        let calorie_count = p.lines().map(|x| x.parse().unwrap()).collect::<Vec<usize>>();
-        let total_calorie: usize = calorie_count.iter().sum();
-        
-        if highest_calorie < total_calorie {
-            highest_calorie = total_calorie
-        }
-    }
-    println!("calorie_count {:?}", highest_calorie);
+    // Part 1&2
+    let mut counts = elfs.map(|elf| {
+            elf.lines().map(|x| x.parse().unwrap()).collect::<Vec<usize>>().iter().sum()
+        })
+        .collect::<Vec<usize>>();
+    
+    counts.sort(); //ascending
+    counts.reverse();
+    println!("Highest calorie {:?}", counts[0]);
+    println!("Sum calorie of top 3 elfs: {}", counts[0]+counts[1]+counts[2]);
 }
