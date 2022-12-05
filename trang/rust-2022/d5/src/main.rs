e6209e57a90b43b9fa4b4bed066b59b831609c62
trangle1302 <lengochaitrang@gmail.com>
2022-12-04 22:23:30 -0800

    d5 p1

--- trang/rust-2022/d5/src/main.rs
+++ trang/rust-2022/d5/src/main.rs
@@ -4,32 +4,35 @@ fn main() {
     let f = std::fs::read_to_string("./input.txt").unwrap();
 
     let mut blocks = f.split("\n\n");
-    let (mut p1, mut p2, mut p3, mut p4, mut p5, mut p6, mut p7, mut p8, mut p9) = (Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new());
-
-    blocks.next().unwrap().lines().rev().skip(1).for_each(|line| {
-        p1.push(line.chars().nth(1).unwrap());
-        p2.push(line.chars().nth(5).unwrap());
-        p3.push(line.chars().nth(9).unwrap());
-        p4.push(line.chars().nth(13).unwrap());
-        p5.push(line.chars().nth(17).unwrap());
-        p6.push(line.chars().nth(21).unwrap());
-        p7.push(line.chars().nth(25).unwrap());
-        p8.push(line.chars().nth(29).unwrap());
-        p9.push(line.chars().nth(33).unwrap());
-    });
-    println!("{:?}", p1);
-    println!("{:?}", p2);
-    p2.retain(|&x| x != ' ');
-    println!("{:?}", p2);
-    p4.retain(|&x| x != ' ');
-    p5.retain(|&x| x != ' ');
-    p7.retain(|&x| x != ' ');
-    p8.retain(|&x| x != ' ');
-    p8.retain(|&x| x != ' ');
     
+    // Read initial stacks blocks
+    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); 9];
+    blocks.next().unwrap().lines().rev().skip(1).for_each(|line| {
+        //for (i, position) in [1,5,9].iter().enumerate() {
+        for (i, position) in [1,5,9,13,17,21,25,29,33].iter().enumerate() {
+            stacks[i].push(line.chars().nth(*position).unwrap())
+        }
+    });
+    for stack in &mut stacks {
+        stack.retain(|&x| x != ' ');
+    }
+    println!("{:?}", stacks);
+    
+    // Move following instructions
     let re_instructions = Regex::new(r"move (?P<n_tomove>\d+) from (?P<p_from>\d+) to (?P<p_to>\d+)").unwrap();
-    let instructions = blocks.next().unwrap().lines().for_each(|line| {
+    blocks.next().unwrap().lines().for_each(|line| {
         let c = re_instructions.captures(&line).unwrap();
-        n_mo = c.name('n_tomove');
+        let n_tomove = c.name("n_tomove").unwrap().as_str().parse::<usize>().unwrap();
+        let from_stack = c.name("p_from").unwrap().as_str().parse::<usize>().unwrap();
+        let to_stack = c.name("p_to").unwrap().as_str().parse::<usize>().unwrap();
+
+        for _ in 0..n_tomove {
+            let letter = *stacks[from_stack -1].last().unwrap();
+            let _ = &mut stacks[to_stack -1].push(letter);
+            let _ = &mut stacks[from_stack -1].pop();
+        }
     }); 
+    
+    let lastletters = stacks.iter().map(|s| *s.last().unwrap()).collect::<String>();
+    println!("{:?}", lastletters);
 }
