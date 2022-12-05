c59a050fee4386c0bec7cbd54ee5d14c26c72ab0
André Danielsson <andre.danielsson@bontouch.com>
2022-12-04 19:05:44 -0800

    Haskell day 1 2022

--- andre/2022/src/Day1.hs
+++ andre/2022/src/Day1.hs
@@ -1,9 +1,19 @@
 module Day1 (day1) where
 
+import Data.List (sortBy)
+import Data.List.Split (splitOn)
+
+elves :: String -> [Int]
+elves = map (sum . map read . lines) . splitOn "\n\n"
+
+part1 :: String -> Int
+part1 = maximum . elves
+
+part2 :: String -> Int
+part2 = sum . take 3 . sortBy (flip compare) . elves
 
 day1 :: IO (Int, Int)
 day1 = do
-    input <- readFile "input/test.txt"
-    putStrLn input
-    return (0, 0)
+    input <- readFile "input/day1.txt"
+    return (part1 input, part2 input)
 
