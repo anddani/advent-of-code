18f44619a8ee4be9f5f04334ad28c98f1d9d3cd1
trangle1302 <lengochaitrang@gmail.com>
2022-12-05 21:31:35 -0800

    d5 p1 p2 cleanup

--- trang/rust-2022/d5/src/main.rs
+++ trang/rust-2022/d5/src/main.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-fn part1() {
+fn main() {
     let f = std::fs::read_to_string("./input.txt").unwrap();
 
     let mut blocks = f.split("\n\n");
@@ -8,7 +8,6 @@ fn part1() {
     // Read initial stacks blocks
     let mut stacks: Vec<Vec<char>> = vec![Vec::new(); 9];
     blocks.next().unwrap().lines().rev().skip(1).for_each(|line| {
-        //for (i, position) in [1,5,9].iter().enumerate() {
         for (i, position) in [1,5,9,13,17,21,25,29,33].iter().enumerate() {
             stacks[i].push(line.chars().nth(*position).unwrap())
         }
@@ -17,6 +16,7 @@ fn part1() {
         stack.retain(|&x| x != ' ');
     }
     println!("{:?}", stacks);
+    let mut stacks_2 = stacks.clone();
     
     // Move following instructions
     let re_instructions = Regex::new(r"move (?P<n_tomove>\d+) from (?P<p_from>\d+) to (?P<p_to>\d+)").unwrap();
@@ -26,56 +26,24 @@ fn part1() {
         let from_stack = c.name("p_from").unwrap().as_str().parse::<usize>().unwrap();
         let to_stack = c.name("p_to").unwrap().as_str().parse::<usize>().unwrap();
 
+        // Part 1
         for _ in 0..n_tomove {
             let letter = *stacks[from_stack -1].last().unwrap();
             let _ = &mut stacks[to_stack -1].push(letter);
             let _ = &mut stacks[from_stack -1].pop();
         }
-    }); 
-    
-    let lastletters = stacks.iter().map(|s| *s.last().unwrap()).collect::<String>();
-    println!("{:?}", lastletters);
-}
 
-
-fn part2() {
-    let f = std::fs::read_to_string("./input.txt").unwrap();
-
-    let mut blocks = f.split("\n\n");
-    
-    // Read initial stacks blocks
-    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); 9];
-    blocks.next().unwrap().lines().rev().skip(1).for_each(|line| {
-        for (i, position) in [1,5,9,13,17,21,25,29,33].iter().enumerate() {
-            stacks[i].push(line.chars().nth(*position).unwrap())
-        }
-    });
-    for stack in &mut stacks {
-        stack.retain(|&x| x != ' ');
-    }
-    println!("{:?}", stacks);
-    
-    // Move following instructions
-    let re_instructions = Regex::new(r"move (?P<n_tomove>\d+) from (?P<p_from>\d+) to (?P<p_to>\d+)").unwrap();
-    blocks.next().unwrap().lines().for_each(|line| {
-        let c = re_instructions.captures(&line).unwrap();
-        let n_tomove = c.name("n_tomove").unwrap().as_str().parse::<usize>().unwrap();
-        let from_stack = c.name("p_from").unwrap().as_str().parse::<usize>().unwrap();
-        let to_stack = c.name("p_to").unwrap().as_str().parse::<usize>().unwrap();
-
+        // Part 2
         let mut letters = Vec::new();
         for _ in 0..n_tomove {
-            letters.push(*stacks[from_stack -1].last().unwrap());
-            let _ = &mut stacks[from_stack -1].pop();
+            letters.push(*stacks_2[from_stack -1].last().unwrap());
+            let _ = &mut stacks_2[from_stack -1].pop();
         }
-        let _ = &mut stacks[to_stack -1].extend(letters.iter().rev());
+        let _ = &mut stacks_2[to_stack -1].extend(letters.iter().rev());
     }); 
-    
+        
     let lastletters = stacks.iter().map(|s| *s.last().unwrap()).collect::<String>();
-    println!("{:?}", lastletters);
-}
-
-fn main() {
-    part1();
-    part2();
+    println!("Part 1: {:?}", lastletters);
+    let lastletters_2 = stacks_2.iter().map(|s| *s.last().unwrap()).collect::<String>();
+    println!("Part 2: {:?}", lastletters_2);
 }
