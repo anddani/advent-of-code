c2170b0a0b95cae2793b712321234acdbc925713
trangle1302 <lengochaitrang@gmail.com>
2022-12-05 21:26:04 -0800

    d6 p1 p2

--- trang/rust-2022/d6/src/main.rs
+++ trang/rust-2022/d6/src/main.rs
@@ -2,12 +2,24 @@ use std::collections::HashSet;
 
 fn main() {
     let f = std::fs::read_to_string("./input.txt").unwrap();
+    // Part 2
     let l = f.chars().count() - 4;
     for i in 0..l {
         let sliding_window = f.chars().skip(i).take(4).collect::<HashSet<char>>();
         if sliding_window.len() == 4 {
             let start_of_pack = f.chars().take(i+4).collect::<String>();
-            println!("{}", start_of_pack.chars().count());
+            println!("Start of pack: {}", start_of_pack.chars().count());
+            break
+        }
+    }
+
+    // Part 2    
+    let l = f.chars().count() - 14;
+    for i in 0..(l+1) {
+        let sliding_window = f.chars().skip(i).take(14).collect::<HashSet<char>>();
+        if sliding_window.len() == 14 {
+            let start_of_message = f.chars().take(i+14).collect::<String>();
+            println!("Start of message: {}", start_of_message.chars().count());
             break
         }
     }
